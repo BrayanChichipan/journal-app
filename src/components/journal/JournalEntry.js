7573458780ae833e4e5dedfe72243b215d5898ed
chichipan <a1567149d@fie.umich.mx>
2021-06-09 17:11:44 -0500

    sidebar v1

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry">
+            <div 
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage:'url(https://www.imagen.com.mx/assets/img/imagen_share.png)'
+
+                }}
+            >
+            </div>
+
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">
+                    Un nuevo dia
+                </p>
+                <p className="journal__entry-content">
+                    este debe se un texto medianamente grando o quiza no tanto
+                </p>
+
+            </div>
+
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+        </div>
+    )
+}
