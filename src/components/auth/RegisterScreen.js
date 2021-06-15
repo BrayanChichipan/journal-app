d71f002e8e47e8cee5ca6c1b6bff4961324f9e75
chichipan <a1567149d@fie.umich.mx>
2021-06-15 14:19:17 -0500

    useFORM EN REGISTERSCREEN

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,19 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
 
 export const RegisterScreen = () => {
+
+    const [formValues, handleInputChange] = useForm({
+        name: 'Hernando',
+        email: 'nando@gmail.com',
+        password: '123456',
+        password2: '123456',
+    });
+
+    const { name, email, password, password2} = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        console.log(formValues)
+    }
+
+    const isFormValid = () => {
+        
+    }
+
     return (
         <>
 
             <h3 className="auth__title">Register</h3>
 
-            <form>
+            <form onSubmit = { handleRegister }>
                 <input
                     type="text"
                     placeholder="Name"
                     name="name"
                     className="auth__input"
                     autoComplete="off"
+                    onChange={ handleInputChange }
+                    value={ name }
                 />
 
                 <input
@@ -22,6 +44,8 @@ export const RegisterScreen = () => {
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    onChange={ handleInputChange }
+                    value={ email }
                 /> 
 
                 <input
@@ -29,13 +53,17 @@ export const RegisterScreen = () => {
                     placeholder="Password"
                     name="password"
                     className="auth__input"
-                />
+                    onChange={ handleInputChange }
+                    value={ password }
+                    />
 
                 <input
                     type="password"
                     placeholder="Confirm"
                     name="password2"
                     className="auth__input"
+                    onChange={ handleInputChange }
+                    value={ password2 }
                 />
 
                 <button
