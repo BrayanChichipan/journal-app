8156313a52758e4e3b941abdd2b25cfd65a6416a
chichipan <a1567149d@fie.umich.mx>
2021-06-15 16:22:39 -0500

    uiReducer: ui.js para manejar el error del form

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom'
+import validator from 'validator';
+import { removeError, setError } from '../../actions/ui';
 import { useForm } from '../../hooks/useForm'
 
 export const RegisterScreen = () => {
+
+    const dispatch = useDispatch();
 
     const [formValues, handleInputChange] = useForm({
         name: 'Hernando',
@@ -15,11 +20,28 @@ export const RegisterScreen = () => {
 
     const handleRegister = (e) => {
         e.preventDefault();
-        console.log(formValues)
+        
+        if(isFormValid()){
+            
+        }
+
     }
 
     const isFormValid = () => {
-        
+
+        if(name.trim().length === 0){
+            dispatch( setError('Name is required') );
+            return false;
+        }else if( !validator.isEmail(email)  ){
+            dispatch( setError('Email is not valid') );
+            return false
+        }else if ( password !== password2 || password.length < 5){
+            dispatch( (setError('Password should be at least 6 characters and match')) );
+            return false;
+        }
+
+        dispatch( removeError() );
+        return true;
     }
 
     return (
@@ -28,6 +50,11 @@ export const RegisterScreen = () => {
             <h3 className="auth__title">Register</h3>
 
             <form onSubmit = { handleRegister }>
+
+                <div className="auth__alert-error">
+                    HOla mundo
+                </div>
+
                 <input
                     type="text"
                     placeholder="Name"
