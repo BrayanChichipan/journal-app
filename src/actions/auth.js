fdee04d14e2daf0f36b0b1a10bd00d87a93315ad
chichipan <a1567149d@fie.umich.mx>
2021-06-15 12:38:11 -0500

    cambios en auth para accion asincrona de ejemplo

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,18 @@
 import { types } from "../types/types"
 
+export const startLoginEmailPassword = (email, password) => {
+      
+    return ( dispatch ) => {
+
+        setTimeout(() => {
+            
+            dispatch( login (1234,'pedro') );
+
+        }, 3500);
+
+    }
+
+}
 
 export const login = ( uid, displayName ) => (
     {
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware,compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import { authReducer } from '../reducers/authReducer';
